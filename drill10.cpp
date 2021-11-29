f7e475ea4d3780f55a2c78898339af73963538fc
angyalsanyi2002 <90507283+angyalsanyi2002@users.noreply.github.com>
2021-11-29 12:54:22 +0100

    Update drill10.cpp

--- drill10.cpp
+++ drill10.cpp
@@ -1,86 +1,70 @@
 #include "std_lib_facilities.h"
-using namespace std;
-
-
 struct Point{
-    int x;
-    int y;
+    double x;
+    double y;
 };
-
-istream& operator>>(istream& istr, Point& p){
-
-    char ch1;
-    if (istr >> ch1 && ch1 != '('){
-        istr.unget();
-        istr.clear(ios_base::failbit);
-    return istr;
-    }
-    char ch2;
-    char ch3;
-    int xx;
-    int yy;
-    istr >> xx >> ch2 >> yy >> ch3;
-    if (!istr || ch3 != ')'){
-        if (istr.eof()) return istr;
-        error("wrong point");
-    }
-    p.x = xx;
-    p.y = yy;
-    return istr;
-}
-
-ostream& operator<< (ostream& ostr, Point& p)
+void compare(vector<Point> original_points, vector<Point> processed_points)
 {
-    return ostr << '(' << p.x << ',' << p.y << ')';
-}
-
-void read_from_file(vector<Point>& points, string& name)
-{
-    ifstream ist {name};
-    ist.exceptions(ist.exceptions()|ios_base::badbit);
-    if (!ist) error("A bemeneti fájlt nem lehet megnyitni.", name);
-    for (Point p; ist >> p;)
-        points.push_back(p);
-}
-int main(){
-try
-{
-     cout << "Please give seven (x,y) pairs!\n";
-     vector<Point> original_points;
-
-     while (original_points.size()<7){
-         Point p;
-         cin >> p;
-         original_points.push_back(p);
-     }
-    for (Point p : original_points)
-        cout << p;
-        cout << '\n';
-    
-    string file_name = "mydata.txt";
-    ofstream ostr { file_name};
-    if (!ostr) error (file_name, "Can't open the file.");
-
-    for (Point p: original_points)
-        ostr << p;
-    ostr.close();
-
-    vector<Point> processed_points;
-    read_from_file(processed_points, file_name);
-
-    for (Point p: processed_points)
-        cout << p;
-        cout << '\n';
-
     if (original_points.size() != processed_points.size())
-        cout << "Something's wrong.\n";
-}
-catch (exception& e){
-    cerr << "Error: " << e.what() << '\n';
-    return 1;
-}
-catch (...){
-    cerr << "Something's wrong.\n";
-    return 2;
-}
+    {
+        cout << "Something's wrong!";
+    }
+    for (int i = 0; i < original_points.size(); i++){
+        if (original_points[i].x != processed_points[i].x){         //orig. és proc. i-edik elemeinek x értékeit hasonlítja össze
+            cout << "Something's wrong!\n";
+
+        }
+        if (original_points[i].y != processed_points[i].y){         //orig. és proc. i-edik elemeinek y értékeit hasonlítja össze
+            cout << "Something's wrong!\n";
+        }
+    }
+};
+int main ()
+{
+   double x;
+   double y;
+   vector<Point> original_points; 
+
+    int value = 0;
+    cout << "Enter values like this: x y";
+    while(value != 7){
+       cin >> x >> y;
+       original_points.push_back(Point{x,y});                   //original_point-ba rakja bele a pontokat
+       value++;
+    }
+
+    string ofname = "mydata.txt";
+    ofstream ost {ofname};
+   // if(!ost) error("Can't open ofile,", ofname);
+    for(int i = 0; i<original_points.size(); i++)
+	{
+		ost<<original_points[i].x<<" "<<original_points[i].y<<endl;          //fájlba kiírja a pontokat, fontos, hogy ne legyen benne zárójel
+	}
+
+    ost.close(); 
+
+    // processed_points létrehozása fájlból
+
+    vector<Point> processed_points; 
+    string ifname = "mydata.txt";
+	ifstream ist {ifname};
+	// if (!ist) error("Can't open input file ", ifname);
+    while(ist >> x >> y){
+    processed_points.push_back(Point{x,y});     //processed_point-ba rakja bele a pontokat
+    }
+    ist.close(); 
+    cout << "original_points: \n";
+     for(int i = 0; i<original_points.size(); i++)
+	{
+		cout<<'('<<original_points[i].x<<", "<<original_points[i].y<<')'<<endl;
+	}
+
+    cout << "processed_points: \n";
+    for(int i = 0; i<processed_points.size(); i++)
+	{
+		cout<<'('<<processed_points[i].x<<", "<<processed_points[i].y<<')'<<endl;
+	}
+
+    compare(original_points, processed_points);             //összehasonlítja a két vectort
+    return 0;
 }
